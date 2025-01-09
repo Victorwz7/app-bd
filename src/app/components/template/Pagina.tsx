48916cc9d37660a240e076f7b338bdf49d8d8ee1
Victorwz7 <victordousseau027@gmail.com>
2025-01-08 23:32:52 -0300

    segundo commit build

--- src/app/components/template/Pagina.tsx
+++ src/app/components/template/Pagina.tsx
@@ -1,14 +1,15 @@
 import Menu from "./Menu";
 
 export interface PaginaProps {
-    children: any
-    className?: string
+    className?: string;
+    children: React.ReactNode;
 }
-export default function Pagina(props: PaginaProps) {
+
+export default function Pagina({ className, children }: PaginaProps) {
     return (
         <div className="flex">
             <Menu />
-            <main className={`flex-1 p-7 ${props.className ?? ''}`}>{props.children}</main>
+            <main className={`flex-1 p-7 ${className ?? ''}`}>{children}</main>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Pagina from "./components/template/Pagina";
 
 export default function Home() {
